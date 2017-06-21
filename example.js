f056dbde7fb2ec8b8c685f8ce99558a2b95aa145
周立人 <zlr@gokuai.cn>
2017-06-21 16:03:32 +0800

    first commit

---
+++ example.js
@@ -0,0 +1,44 @@
+/**
+ * Created by michael on 2017/6/21.
+ */
+const cos = require("./index");
+
+let c = new cos.CosUtil({
+    AppId: '',
+    SecretId: '',
+    SecretKey: ''
+});
+
+let flat = false;
+setTimeout(() => flat = true, 3000);
+
+c.sliceUploadFile({
+    Bucket: "",
+    Region: "",
+    Key: "",
+    UploadId: ""
+}, {
+    fileName: "",
+    fileSize: 19100116,
+    sliceSize: 1 << 20,
+}, {
+    onProgress: function (a) {
+        console.log(a)
+    },
+    cancel: function () {
+        return flat
+    }
+}).then((a) => {
+    console.log(a);
+}, (err) => {
+    console.log(err);
+});
+
+//直接调用cos-nodejs-sdk方法
+c.cos.getBucket({
+    Bucket: "",
+    Region: "",
+    Delimiter: "/",
+}, (err, data) => {
+    console.log(err, data)
+});
---
+++ index.test.js
@@ -0,0 +1,15 @@
+/**
+ * Created by michael on 2017/6/21.
+ */
+const cos = require("./index");
+
+const assert = require("assert");
+
+describe('a test', function () {
+
+    it('', function (done) {
+
+
+
+    });
+});
